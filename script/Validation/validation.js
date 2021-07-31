4c780066969021923fe6aba5614059a207d65770
Dima-GT <dimon200117@gmail.com>
2021-08-01 02:05:12 +0300

    full valid

--- script/Validation/validation.js
+++ script/Validation/validation.js
@@ -1,5 +1,9 @@
 let regexEmail = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/,
-    regexName = /^[a-zа-яё]{1,25}$/i;
+    regexName = /^[a-zа-яё]{1,25}$/i,
+    nameValid = false,
+    emailValid = false,
+    phoneValid = false,
+    areaValid = false;
 
 function validateName(e) {
     let element = document.createElement("div");
@@ -11,11 +15,13 @@ function validateName(e) {
         error !== undefined ? error.remove() : null
         e.classList.remove('uncorrect')
         e.classList.add('correct')
+        nameValid = true
     } else {
         error !== undefined ? error.remove() : null
         e.classList.remove('correct')
         e.classList.add('uncorrect')
         document.getElementById("name").appendChild(element);
+        nameValid = false
     }
 }
 
@@ -29,10 +35,37 @@ function validateEmail(e) {
         error !== undefined ? error.remove() : null
         e.classList.remove('uncorrect')
         e.classList.add('correct')
+        emailValid = true
     } else {
         error !== undefined ? error.remove() : null
         e.classList.remove('correct')
         e.classList.add('uncorrect')
         document.getElementById("email").appendChild(element);
+        emailValid = false
     }
 }
+
+function validatePhone(e) {
+    setTimeout(() => e.classList.contains("correct") ? phoneValid = true : null , 10 )
+
+}
+function validateArea(e) {
+    if (e.value !== ''){
+        e.classList.remove('uncorrect')
+        e.classList.add('correct')
+        areaValid = true
+    } else {
+        e.classList.remove('correct')
+        e.classList.add('uncorrect')
+        areaValid = false
+    }
+}
+function formCheck() {
+    !nameValid ? document.getElementById("nameInput").classList.add('uncorrect') : null
+    !emailValid ? document.getElementById("emailInput").classList.add('uncorrect') : null
+    !phoneValid ? document.getElementById("phone").classList.add('uncorrect') : null
+    !areaValid ? document.getElementById("area").classList.add('uncorrect') : null
+
+    return nameValid && emailValid && phoneValid && areaValid;
+}
+
