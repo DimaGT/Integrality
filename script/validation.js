97cd862bee5b9309dc2d8ce86d76a6c402baf4b0
Dima-GT <dimon200117@gmail.com>
2021-07-31 16:38:22 +0300

    validation

--- script/validation.js
+++ script/validation.js
@@ -1,14 +1,14 @@
 let regexEmail = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/,
-    regexName = /^[a-zA-Zа-яёА-ЯЁ].{1,25}$/;
+    regexName = /^[a-zа-яё]{1,25}$/i;
 
 function validateName(e) {
-    let element = document.createElement("div"),
-        textnode = document.createTextNode("Name is not valid");
+    let element = document.createElement("div");
+    let textnode = document.createTextNode("Name is not valid");
     element.className = "error name-error";
     element.appendChild(textnode);
     let error = document.getElementsByClassName('name-error')[0];
     if (regexName.test(e.value)) {
-         error !== undefined ? error.remove() : null
+        error !== undefined ? error.remove() : null
         e.classList.remove('uncorrect')
         e.classList.add('correct')
     } else {
@@ -16,15 +16,23 @@ function validateName(e) {
         e.classList.remove('correct')
         e.classList.add('uncorrect')
         document.getElementById("name").appendChild(element);
-        return false;
     }
 }
 
-function validateEmail(value) {
-    if (regexEmail.test(value)) {
-        alert("good");
+function validateEmail(e) {
+    let element = document.createElement("div");
+    let textnode = document.createTextNode("Email is not valid");
+    element.className = "error email-error";
+    element.appendChild(textnode);
+    let error = document.getElementsByClassName('email-error')[0];
+    if (regexEmail.test(e.value)) {
+        error !== undefined ? error.remove() : null
+        e.classList.remove('uncorrect')
+        e.classList.add('correct')
     } else {
-        alert('huevo')
-        return false;
+        error !== undefined ? error.remove() : null
+        e.classList.remove('correct')
+        e.classList.add('uncorrect')
+        document.getElementById("email").appendChild(element);
     }
 }
